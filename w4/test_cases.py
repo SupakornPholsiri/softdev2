69f24719854f42de059174cb9b42a998acf93913
MessPh <math7064@gmail.com>
2023-01-10 22:24:03 +0700

    Update

--- w4/test_cases.py
+++ w4/test_cases.py
@@ -16,7 +16,7 @@ class Index_test(unittest.TestCase):
                         {"she":["test"], "ate":["test"], "a":["test"], "dog":["test"], "he":["test"], "cat":["test"]})
 
 class Spider_test(unittest.TestCase):
-
+    
     @patch("general_scrape_test_w4.Spider.queue", new=list())
     def test_spider_init(self):
         scraper = Spider("https://www.dummy.com")
@@ -36,14 +36,50 @@ class Spider_test(unittest.TestCase):
             scraper.generate_soup("https://www.dummy.com")
             assert scraper.url == "https://www.dummy.com"
             assert scraper.soup == BeautifulSoup(mock_get.return_value.text, "html.parser")
-
+            
+    @patch("general_scrape_test_w4.Spider.crawled", new=list())
     @patch("general_scrape_test_w4.Spider.queue", new=list())
     @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.test.co.th","https://www.dummy.com"})
     def test_add_links_to_queue(self, mock_get_links):
         scraper = Spider("https://www.not_used.com")
         scraper.add_links_to_queue()
         assert sorted(Spider.queue) == sorted(["https://www.test.co.th", "https://www.dummy.com", "https://www.not_used.com"])
-
+        
+    @patch("general_scrape_test_w4.Spider.crawled", new=["https://www.mommy.com"])
+    @patch("general_scrape_test_w4.Spider.queue", new=["https://www.daddy.com"])
+    @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.mommy.com"})
+    def test_add_links_to_queue2(self, mock_get_links):
+        scraper = Spider("https://www.not_used.com")
+        scraper.add_links_to_queue()
+        assert sorted(Spider.queue) == sorted(["https://www.daddy.com","https://www.not_used.com"])
+        
+    @patch("general_scrape_test_w4.Spider.crawled", new=["https://www.messy.com"])
+    @patch("general_scrape_test_w4.Spider.queue", new=["https://www.Nunounuing.com"])
+    @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.Nunounuing.com"})
+    def test_add_links_to_queue3(self, mock_get_links):
+        scraper = Spider("https://www.not_used.com")
+        scraper.add_links_to_queue()
+        assert sorted(Spider.queue) == sorted(["https://www.Nunounuing.com","https://www.not_used.com"])
+        
+    @patch("general_scrape_test_w4.Spider.crawled", new=["https://www.Ronaldo.com"])
+    @patch("general_scrape_test_w4.Spider.queue", new=["https://www.Taric.com"])
+    @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.Yasuo.com"})
+    def test_add_links_to_queue4(self, mock_get_links):
+        scraper = Spider("https://www.Yasuo.com")
+        scraper.add_links_to_queue()
+        assert sorted(Spider.queue) == sorted(["https://www.Taric.com","https://www.Yasuo.com"])
+        
+    @patch("general_scrape_test_w4.Spider.crawled", new=["https://www.Ronaldo.com","https://www.Messy.com","https://www.Mbape.com","https://www.Aguero.com"])
+    @patch("general_scrape_test_w4.Spider.queue", new=["https://www.Lukaku.com","https://www.Modric.com","https://www.Darwin.com"])
+    @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.Ronaldo.com","https://www.Messy.com","https://www.Aguero.com"})
+    def test_add_links_to_queue5(self, mock_get_links):
+        scraper = Spider("https://www.Yasuo.com")
+        scraper.add_links_to_queue()
+        assert sorted(Spider.queue) == sorted(["https://www.Yasuo.com","https://www.Lukaku.com","https://www.Modric.com","https://www.Darwin.com"])
+        
+        
+        
+    @patch("general_scrape_test_w4.Spider.crawled", new=list())
     @patch("general_scrape_test_w4.Spider.queue", new=["https://Softdev.co.th", "https:Softdev.co.th/Help/me"])
     @patch("general_scrape_test_w4.Spider.get_links", return_value = {"https://www.test.co.th","https://www.dummy.com"})
     def test_add_links_to_exsiting_queue(self, mock_get_links):
