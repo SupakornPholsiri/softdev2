2e6dc9b1f57fce29c7a1eeefe20e5d776a8a44a6
MessPh <math7064@gmail.com>
2023-02-14 22:17:45 +0700

    tokenized new

---
+++ Tokenize.py
@@ -0,0 +1,36 @@
+from pythainlp import word_tokenize
+import emoji
+import re
+
+test = "Yesasdasdasd Baby Study Studying studied 😀 13216546541323 หัวเราะนะครับ"
+class Tokenize:
+    def __init__(self) -> None:
+         self.tokens = ""
+    def tokenize(self,tokens):
+        re1 = re.sub("[0-9]|[+,-,*,/]","",tokens)
+        print(re1)
+        # tokens = re.findall(r'\b\w+\b', re1)
+        # print(tokens)
+        afteremoji = ""
+        for i in re1:
+            if i in emoji.EMOJI_DATA:
+                afteremoji += ""
+            else:
+                afteremoji += i
+        print(afteremoji)
+        tokenized = word_tokenize(afteremoji)
+        finaltokenized = []
+        for x in tokenized:
+            if x.startswith(" ") :
+                continue
+            else:
+                finaltokenized.append(x)
+        return finaltokenized
+ex = Tokenize()
+print(ex.tokenize(test))
+
+
+    
+
+        
+        
