eff2fb4d446fc40232812977c039fc91a8f965d2
MessPh <math7064@gmail.com>
2023-02-17 14:15:31 +0700

    update token

--- Rework/Tokenize.py
+++ Rework/Tokenize.py
@@ -2,32 +2,32 @@ from pythainlp import word_tokenize
 import emoji
 import re
 
-test = "Yesasdasdasd Baby Study Studying studied 😀 D-13216546541323D หัวเราะนะครับ"
+test = "Yesasdasdasd Baby-have Study Studying studied 😀 13216546541323 หัวเราะนะครับ"
 class Tokenize:
     def __init__(self) -> None:
          self.tokens = ""
     def tokenize(self,tokens):
-        re1 = re.sub("[0-9]|[+\-*/]|[\t\n]"," ",tokens)
+        re1 = re.sub(r'[][!-@#$?%+:"\n^_]'," ",tokens)
+        # print(re1)
         # tokens = re.findall(r'\b\w+\b', re1)
-        # print(tokens)
+        print(re1)
         afteremoji = ""
         for i in re1:
             if i in emoji.EMOJI_DATA:
-                afteremoji += " "
+                afteremoji += ""
             else:
                 afteremoji += i
+        # print(afteremoji)
         tokenized = word_tokenize(afteremoji)
         finaltokenized = []
         for x in tokenized:
-            if x.startswith(" ") or x == "\xa0":
+            if x.startswith(" ") :
                 continue
             else:
                 finaltokenized.append(x)
         return finaltokenized
-
-if __name__ == "__main__":
-    ex = Tokenize()
-    print(ex.tokenize(test))
+ex = Tokenize()
+print(ex.tokenize(test))
 
 
     
