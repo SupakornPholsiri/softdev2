dd81c6155163f0e2d083fccb2cd87b1725e07ab0
SupakornPholsiri <supakorncpw@gmail.com>
2023-02-17 14:02:09 +0700

    Updated Index

--- Rework/IndexV2.py
+++ Rework/IndexV2.py
@@ -6,7 +6,7 @@ from collections import Counter
 class Database:
     def __init__(self):
         self.client = MongoClient('localhost:27017')
-        self.SearchEngine = self.client['SearchEngine']
+        self.SearchEngine = self.client['TestSearchEngine']
         self.raw_data_storage = self.SearchEngine["RawData"]
         self.dbweb = self.SearchEngine['WebDB']
 
@@ -23,8 +23,8 @@ class RawInfoIndex:
         elif self.index[url]["hash"] != hash:
             self.index[url] = {"text":raw_text, "links":links, "hash":hash}
             self.url_to_be_updated.add(url)
-        else :
-            self.index[url] = {"text":raw_text, "links":links, "hash":hash}
+        """else :
+            self.index[url] = {"text":raw_text, "links":links, "hash":hash}"""
         return self.index
 
     def save_to_database(self, database):
@@ -68,6 +68,7 @@ class InvertedIndex:
 
     def __init__(self):
         self.index = {}
+        self.keywords_to_be_updated = set()
 
     """ #Add or add onto keyword index using the tokens.
     def modify_index_with_tokens(self, tokens, url):
@@ -91,16 +92,17 @@ class InvertedIndex:
 
     #Add or add onto keyword index using the tokens.
     def modify_index_with_tokens(self, tokens, url):
-        #Pattern for removing most punctuations and special characters tokens
         counter = Counter(tokens)
         for token in tokens:
-            #Remove None, punctuations and special characters tokens
             if token not in self.index :
                 self.index[token] = {url:counter[token]}
             elif url not in self.index[token]:
                 self.index[token][url] = counter[token]
-            elif url in self.index[token] and self.index[token][url] != counter[token]:
+            elif self.index[token][url] != counter[token]:
                 self.index[token][url] = counter[token]
+            else:
+                continue
+            self.keywords_to_be_updated.add(token)
         return self.index
 
     """#Save current index to csv file
@@ -122,10 +124,12 @@ class InvertedIndex:
     def save_to_database(self, database):
         inverted_collection = database["WebDB"]
         for keyword in self.index:
-            if inverted_collection.find_one({"key":keyword}):
-                inverted_collection.find_one_and_update({"key":keyword},{"$set":{"value":self.index[keyword]}})
-            else:
-                inverted_collection.insert_one({"key":keyword},{"$set":{"value":self.index[keyword]}})
+            if keyword in self.keywords_to_be_updated:
+                if inverted_collection.find_one({"key":keyword}):
+                    inverted_collection.find_one_and_update({"key":keyword},{"$set":{"value":self.index[keyword]}})
+                else:
+                    inverted_collection.insert_one({"key":keyword, "value":self.index[keyword]})
+                self.keywords_to_be_updated.remove(keyword)
 
     def read_from_database(self, database):
         inverted_collection = database["WebDB"]
@@ -136,21 +140,47 @@ class ForwardIndex :
 
     def __init__(self):
         self.index = {}
+        self.urls_to_be_updated = set()
+        self.locations = []
 
-    def f():
-        pass
+    def get_location_info(self, url, tokens):
+        if url not in self.index:
+            self.index[url] = {}
+        self.index[url]["Location"] = {}
+        for token in tokens:
+            if token in self.locations:
+                self.index[url]["Location"][f"{len(self.index[url]['Location'])-1}"] = token
 
-    #Add ref count
-    def count_reference(self, link):
-        if link not in self.ref_index:
-            self.ref_index[link] = 1
-        else:
-            self.ref_index[link] += 1
-
-    #Save current index to csv file
-    def save_to_file(self):
-        with open('ref_index.csv', 'w', encoding="utf-8") as f:
-            for key in self.ref_index.keys():
-                f.write(f'"{key}","{self.ref_index[key]}"\n')
-        f.close()
+    def modify_ref_count(self, url:str, links:list[str], base_domains:list[str]):
+        for link in links:
+            print(link)
+            domain = link.split("/")[2]
+            if domain not in base_domains or domain == url.split("/")[2]:
+                continue
+            self.count_reference(url)
     
+    #Add ref count
+    def count_reference(self, url:str):
+        if url not in self.index:
+            self.index[url]["Refcount"] = 1
+        else:
+            self.index[url]["RefCount"] += 1
+
+    def modify_index(self, url:str, links:list[str], tokens:list[str], base_domains:list[str]):
+        location = self.get_location_info(url, tokens)
+        self.modify_ref_count(url, links, base_domains)
+        if url not in self.index:
+            self.index[url]["Location"] = location
+        elif self.index[url]["Location"] != location:
+            self.index[url]["Location"] = location
+        else:
+            return self.index
+        return self.index
+
+    def save_to_database(self, database):
+        forward_database = database["FWIndex"]
+        for url in self.urls_to_be_updated:
+            pass
+
+    def read_from_database(self, database):
+        pass
