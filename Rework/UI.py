e479f1b4f4b1e9e94d3b3e123d2996d95723d2a0
MessPh <math7064@gmail.com>
2023-03-11 20:47:00 +0700

    update

--- Rework/UI.py
+++ Rework/UI.py
@@ -1,20 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QListWidget, QListWidgetItem
 
-def window():
-   app = QApplication(sys.argv)
-   widget = QWidget()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-   textLabel = QLabel(widget)
-   textLabel.setText("Hello World!")
-   textLabel.move(110,85)
+        # Set window properties
+        self.setWindowTitle("Search Bar Example")
+        self.setGeometry(100, 100, 800, 600)
 
-   widget.setGeometry(50,50,320,200)
-   widget.setWindowTitle("PyQt5 Example")
-   widget.show()
-   sys.exit(app.exec_())
+        # Create main widget
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
 
-if __name__ == '__main__':
-   window()
+        # Create search bar and buttons
+        search_bar = QLineEdit()
+        search_button = QPushButton("Search")
+        graph_button = QPushButton("Graph Spatial")
+        graph_frequency_button = QPushButton("Graph Frequency")
+
+        # Create layout for search bar and buttons
+        search_layout = QHBoxLayout()
+        search_layout.addWidget(search_bar)
+        search_layout.addWidget(search_button)
+        search_layout.addWidget(graph_button)
+        search_layout.addWidget(graph_frequency_button)
+
+        # Create list widget to display search results
+        self.list_widget = QListWidget()
+
+        # Create layout for main widget
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(search_layout)
+        main_layout.addWidget(self.list_widget)
+
+        # Set main widget layout
+        main_widget.setLayout(main_layout)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
